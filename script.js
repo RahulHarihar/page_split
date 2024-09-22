173a69eaba20c4e9d0743df25f01c659b7aa529b
unknown <rahulharihar71@gmail.com>
2024-09-22 23:20:21 +0530

    implemented smooth scrolling effect

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+	console.log(e);
+});
+
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 var timeLine = gsap.timeline({
 	scrollTrigger: {
 		trigger: ".main",
